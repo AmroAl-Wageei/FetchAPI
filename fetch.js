b284b96b12e8c379e0f7084387a5cfa7ee9af1af
Amro Al-Wageei <shandaqamro@gmail.com>
2023-03-29 12:13:24 +0300

    fetch

---
+++ fetch.js
@@ -0,0 +1,18 @@
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const userFile = document.getElementById("file").files[0];
+  const userComment = document.getElementById("comments").value;
+
+  const formData = new FormData();
+  formData.append("user-file", userFile, "user-file.jpg");
+  formData.append("user-comments", userComment);
+
+  fetch("https://httpbin.org/post", {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+});
